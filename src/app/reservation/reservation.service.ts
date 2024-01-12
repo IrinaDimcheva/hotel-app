601af5fb0c830ce33be81d17df5b3fbb831bb4f0
Irina Dimcheva <dimchevairina@gmail.com>
2024-01-12 23:53:28 +0200

    Implement reservation service

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ReservationService {
+  private reservations: Reservation[] = [];
 
-  constructor() { }
+  getReservations(): Reservation[] {
+    return this.reservations;
+  }
+
+  getReservation(id: string): Reservation | undefined {
+    return this.reservations.find((res) => res.id === id);
+  }
+
+  addReservation(reservation: Reservation): void {
+    this.reservations.push(reservation);
+  }
+
+  deleteReservation(id: string): void {
+    this.reservations = this.reservations.filter((res) => res.id !== id);
+  }
+
+  updateReservation(updatedReservation: Reservation): void {
+    const index = this.reservations.findIndex(
+      (res) => res.id === updatedReservation.id
+    );
+    this.reservations[index] = updatedReservation;
+  }
 }
